8db45c20ca901a8bc6187f822413e055117deeef
Jonathan Manning <jmanning@ebi.ac.uk>
2019-08-05 10:11:17 +0100

    Point at final application URI

--- deployment-guides/AWS-EKS-EFS/deploy-all.sh
+++ deployment-guides/AWS-EKS-EFS/deploy-all.sh
@@ -32,3 +32,9 @@ deploy_efs_shared_fs.sh
 helm repo add galaxy-helm-repo https://pcm32.github.io/galaxy-helm-charts
 helm install -f $SCRIPT_DIR/helm-hinxton-single-cell-aws.yaml galaxy-helm-repo/galaxy-stable
 
+# Print application URI
+
+ip=$(aws ec2 describe-instances --region $AWS_REGION --filters "[{\"Name\": \"vpc-id\",\"Values\": [\"$vpc_id\"]}]" --query "Reservations[0].Instances[].NetworkInterfaces[].Association.PublicIp" --output text)
+port=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services flailing-lionfish-galaxy-stable)
+
+echo "Startup may take some time. Galaxy application will eventually be available at http://$ip:$port"
