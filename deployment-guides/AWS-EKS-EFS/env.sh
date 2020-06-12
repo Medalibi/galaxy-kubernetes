edfc7ad1483201adcdcf51d039703f99b5fe1712
Mohamed <alibimohamed@gmail.com>
2020-06-12 14:31:04 +0100

    Update env.sh
    Updates names and AWS region

--- deployment-guides/AWS-EKS-EFS/env.sh
+++ deployment-guides/AWS-EKS-EFS/env.sh
@@ -1,7 +1,7 @@
 export DEVELOPER=jon.manning.ebi
-export AWS_REGION=us-west-2
-export CLUSTER_NAME=efs-csi-driver
-export SECURITY_GROUP_NAME=eks_efs_security_group
+export AWS_REGION=eu-west-2
+export CLUSTER_NAME=galaxy-eks-srv
+export SECURITY_GROUP_NAME=galaxy-eks_efs_sec_grp
 export FILESYSTEM_NAME=eks_efs_fs
 export DEPLOYMENT_FOLDER=$(pwd)/deploy
 
