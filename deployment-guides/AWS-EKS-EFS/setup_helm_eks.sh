8e6f7bff7bf30a53c71560711c319e39a5d6b502
Mohamed <alibimohamed@gmail.com>
2020-06-17 13:56:34 +0100

    Update setup_helm_eks.sh
    fixing tiller init

--- deployment-guides/AWS-EKS-EFS/setup_helm_eks.sh
+++ deployment-guides/AWS-EKS-EFS/setup_helm_eks.sh
@@ -6,29 +6,15 @@
 
 # TODO check versions of kubectl and helm that are compatible with IAM authenticator
 
-helm_rbac_config=$DEPLOYMENT_FOLDER/helm_rbac.yaml
-cat >$helm_rbac_config <<EOF
----
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: tiller
-  namespace: kube-system
----
-apiVersion: rbac.authorization.k8s.io/v1beta1
-kind: ClusterRoleBinding
-metadata:
-  name: tiller
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-  - kind: ServiceAccount
-    name: tiller
-    namespace: kube-system
-EOF
+helm init
 
-kubectl apply -f $helm_rbac_config
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy --patch '{"spec": {"template": {"spec": {"serviceAccount": "tiller"} } } }'
 
+helm init --service-account tiller --upgrade
 helm init --service-account tiller
+
+git clone https://github.com/galaxyproject/galaxy-helm.git
+cd galaxy-helm/galaxy
+helm dependency update
