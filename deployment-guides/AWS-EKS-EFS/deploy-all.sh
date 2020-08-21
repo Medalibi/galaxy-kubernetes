122abd7fcf6b89c0d145f0e13f0a4f4863efd597
Mohamed <alibimohamed@gmail.com>
2020-08-21 13:26:31 +0100

    Update deploy-all.sh
    configure helm install and remove ip access

--- deployment-guides/AWS-EKS-EFS/deploy-all.sh
+++ deployment-guides/AWS-EKS-EFS/deploy-all.sh
@@ -1,35 +1,123 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export PATH=$(pwd):$SCRIPT_DIR:$PATH
+echo "1. Creating EFS file system"
 
-source $SCRIPT_DIR/env.sh
+aws efs create-file-system --creation-token "$FILESYSTEM_NAME" --performance-mode maxIO --throughput-mode bursting --region $AWS_REGION --tags Key=Name,Value="Galaxy filesystem" Key=developer,Value="$DEVELOPER"
+fs_id=$(aws efs --region $AWS_REGION describe-file-systems --creation-token $FILESYSTEM_NAME --query "FileSystems[0].FileSystemId" --output text)
 
-# Deploy the base kubernetes cluster
+echo -e "Created file system ID $fs_id"
 
-deploy_eks_cluster.sh
+# Derive networking info from the cluster
+
+vpc_id=$(aws ec2 describe-vpcs --output text --region $AWS_REGION --filters "Name=isDefault,Values=true" --query "Vpcs[0].VpcId" --query "Vpcs[0].VpcId")
+vpc_cidr=$(aws ec2 describe-vpcs --output text --region $AWS_REGION --filters "Name=isDefault,Values=true" --query "Vpcs[0].VpcId" --query "Vpcs[0].CidrBlock")
+# Create a security group
+
+security_group_id=$(aws ec2 create-security-group --group-name $SECURITY_GROUP_NAME --vpc-id ${vpc_id} --description "EFS Security Group" --query "GroupId" --output text --region $AWS_REGION)
+if [ $? -eq 0 ]; then
+    echo -e "Created security group with ID $security_group_id"
+else
+    echo "Could not create security group" 1>&2
+    exit 2
+fi
+
+# Add an ingress rule that opens up port 2049 from the 192.168.0.0/16 CIDR range:
+
+aws ec2 authorize-security-group-ingress --group-id ${security_group_id} --protocol tcp --port 2049 --cidr $vpc_cidr --region $AWS_REGION
+aws ec2 authorize-security-group-ingress --group-id ${security_group_id} --protocol tcp --port 2049 --cidr $GALAXY_EKS_SG_ID --region $AWS_REGION
+echo "2. Creating mount targets for each subnet"
+
+subnet_id=$(aws ec2 describe-subnets --filters "[{\"Name\": \"vpc-id\",\"Values\": [\"$vpc_id\"]}]" --region $AWS_REGION --query "Subnets[0].SubnetId" --output text)
+
+aws efs create-mount-target --file-system-id $fs_id --subnet-id $subnet_id --security-group $security_group_id --region $AWS_REGION
+if [ $? -eq 0 ]; then
+    mount_target_id=$(aws efs describe-mount-targets --region $AWS_REGION --file-system-id $fs_id --query "MountTargets[0].MountTargetId" --output text)
+    echo "Created mount target with ID $mount_target_id"
+else
+    echo "Could not create mount target" 1>&2
+    exit 3
+fi
+
+# The mount targets take a little while to create
+
+mount_targets_ready=no
+mt_wait_loops=1
+max_mt_wait_loops=10
+echo "Waiting for mount points to be available"
+
+while [ "$mount_targets_ready" = 'no' ]; do
+
+    loop_mt_ready=yes
+    mount_target_states=$(aws efs describe-mount-targets --region $AWS_REGION --file-system-id $fs_id --query "MountTargets[].LifeCycleState" --output text)
+
+    for mts in $mount_target_states; do
+        if [ "$mts" != 'available' ]; then
+            loop_mt_ready=no
+            break
+        fi
+    done
+
+    if [ "$loop_mt_ready" = 'yes' ]; then
+        mount_targets_ready=yes
+    else
+        echo -n '.'
+        sleep 10
+        mt_wait_loops=$[$mt_wait_loops+1]
+    fi
+done
+
+echo "Mount points ready"
+
+echo "3. Provisioning persisent volumes"
+
+helm install stable/efs-provisioner --set efsProvisioner.efsFileSystemId=$fs_id --set efsProvisioner.awsRegion=$AWS_REGION
+
+sleep 5
+
+kubectl get pods --field-selector status.phase=Running | grep "efs-provisioner"
 
 if [ $? -ne 0 ]; then
-    echo "Unable to create cluster" 1>&2
+    echo "EFS helm deployment failed"
     exit 1
 fi
 
-# Setup Helm
+# setup the efs claim volume
+pvc=$DEPLOYMENT_FOLDER/claim.yaml
+cat >$pvc <<EOF
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: efs-claim
+spec:
+  accessModes:
+    - ReadWriteMany
+  storageClassName: efs
+  resources:
+    requests:
+      storage: 1000Gi
+EOF
 
-setup_helm_eks.sh
+kubectl apply -f $pvc
 
-# Deploy the EFS provisioner
+# Setup the Postgres volume claim on aws gp2
+postgrespvc=$DEPLOYMENT_FOLDER/postgres-claim.yaml
+cat >$postgrespvc <<EOF
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: postgres-pvc
+  annotations:
+    volume.beta.kubernetes.io/storage-class: gp2
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 100Gi
+EOF
 
-deploy_efs_shared_fs.sh
+kubectl apply -f $postgrespvc
 
-# Do the Galaxy setup
+kubectl patch storageclass gp2 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
-helm repo add galaxy-helm-repo https://pcm32.github.io/galaxy-helm-charts
-helm install -f $SCRIPT_DIR/helm-hinxton-single-cell-aws.yaml galaxy-helm-repo/galaxy-stable
-
-# Print application URI
-
-ip=$(aws ec2 describe-instances --region $AWS_REGION --filters "[{\"Name\": \"vpc-id\",\"Values\": [\"$vpc_id\"]}]" --query "Reservations[0].Instances[].NetworkInterfaces[].Association.PublicIp" --output text)
-port=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services flailing-lionfish-galaxy-stable)
-
-echo "Startup may take some time. Galaxy application will eventually be available at http://$ip:$port"
+echo "Use galaxy.pvc=$pvc on helm"
