c5f6aec3bbf4d7770ff70f6d3692563a7e06fdeb
Mohamed <alibimohamed@gmail.com>
2020-08-21 13:35:04 +0100

    Update env.sh
    add ssh key

--- deployment-guides/AWS-EKS-EFS/env.sh
+++ deployment-guides/AWS-EKS-EFS/env.sh
@@ -1,7 +1,8 @@
 export DEVELOPER=jon.manning.ebi
-export AWS_REGION=us-west-2
-export CLUSTER_NAME=efs-csi-driver
-export SECURITY_GROUP_NAME=eks_efs_security_group
-export FILESYSTEM_NAME=eks_efs_fs
+export AWS_REGION=eu-west-2
+export CLUSTER_NAME=galaxy-eks
+export SECURITY_GROUP_NAME=galaxy_eks_efs_sg
+export FILESYSTEM_NAME=galaxy_eks_efs_fs
 export DEPLOYMENT_FOLDER=$(pwd)/deploy
+export AWS_EC2_ACCESS_KEY=Traning
 
